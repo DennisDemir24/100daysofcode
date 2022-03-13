e81f981c77ef11578fc60e89690890051a075b80
Dennis Demir <dd222gc@student.lnu.se>
2022-03-13 18:30:25 +0100

    Desktop design and functionality done

--- AdviceGenerator/advice-generator/src/components/AdviceBox.jsx
+++ AdviceGenerator/advice-generator/src/components/AdviceBox.jsx
@@ -1,12 +1,19 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import PatternDesktop from '../images/pattern-divider-desktop.svg'
+import DiceIcon from '../images/icon-dice.svg'
+import '../App.css'
 
 const AdviceBox = () => {
     const [adviceData, setAdviceData] = useState(null)
 
     const fetchData = async () => {
-      const res = await axios.get('https://api.adviceslip.com/advice')
-      setAdviceData(res.data.slip)
+      try {
+          const res = await axios.get('https://api.adviceslip.com/advice')
+          setAdviceData(res.data.slip)
+      } catch (error) {
+          console.log(error)
+      }
     }
 
     useEffect(() => {
@@ -18,9 +25,16 @@ const AdviceBox = () => {
     <>
       <div className='advice-box'>
         {adviceData && (
-                <div>
-                    <h1>{adviceData.advice}</h1>
-                </div>
+          <div className='inner-box'>
+            <span>Advice #{adviceData.id}</span>
+            <h1>"{adviceData.advice}"</h1>
+            <img src={PatternDesktop} alt='' />
+            <div>
+              <button className='icon-wrapper' onClick={fetchData}>
+                <img src={DiceIcon} className='dice-icon' alt='' />
+              </button>
+            </div>
+          </div>
         )}
       </div>
     </>
