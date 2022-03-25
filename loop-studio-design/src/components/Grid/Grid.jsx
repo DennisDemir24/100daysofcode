57735d957b3568307327b17968793a76a597e447
Dennis Demir <dd222gc@student.lnu.se>
2022-03-25 21:44:08 +0100

    main content and grid

--- loop-studio-design/src/components/Grid/Grid.jsx
+++ loop-studio-design/src/components/Grid/Grid.jsx
@@ -1,8 +1,33 @@
 import './Grid.css'
 
+import DeepEarth from '../../images/desktop/image-deep-earth.jpg'
+
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <div className="container">
+      <div className="title-wrapper">
+        <h1>OUR CREATIONS</h1>
+        <button>SEE ALL</button>
+      </div>
+      <div className="grid-container">
+        <div className="grid-item">
+            <img src={DeepEarth} alt="" />
+            <p>DEEP EARTH</p>
+        </div>
+        <div className="grid-item">
+            <img src={DeepEarth} alt="" />
+            <p>DEEP EARTH</p>
+        </div>
+        <div className="grid-item">
+            <img src={DeepEarth} alt="" />
+            <p>DEEP EARTH</p>
+        </div>
+        <div className="grid-item">
+            <img src={DeepEarth} alt="" />
+            <p>DEEP EARTH</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- loop-studio-design/src/components/Main/MainContent.jsx
+++ loop-studio-design/src/components/Main/MainContent.jsx
@@ -3,11 +3,22 @@ import Image from '../../images/desktop/image-interactive.jpg'
 
 const MainContent = () => {
   return (
-    <div className="main-content">
-        <div className="right">
-            <img src={Image} alt="" />
+    <div className='main-content'>
+      <div className='right'>
+        <img src={Image} alt='' />
+      </div>
+      <div className='left'>
+        <div className='left-content'>
+          <h1>THE LEADER IN INTERACTIVE VR</h1>
+          <p>
+            {' '}
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
         </div>
-        <div className="left"></div>
+      </div>
     </div>
   )
 }
